a5c4d58e2391f672f9725ab04b1bae4965a2c69a
RhazzXIX <jm21tonio@gmail.com>
2023-01-29 18:22:46 +0800

    Add functions to check passwords input.
    Created function checkPassword pattern to constraint the user to create
    a password the includes small/capital letters, numbers and special
    characters.
    Created function confirmPassword to check if the value of the initial
    password and confirm password are the same.

--- src/index.js
+++ src/index.js
@@ -1,67 +1,104 @@
-import { validate } from 'schema-utils';
-import './style/style.css'
+import { format } from "prettier";
+import { validate } from "schema-utils";
+import "./style/style.css";
 
 // DOM
 
 const body = document.querySelector("body");
-const button = document.querySelector('button')
-const fName = document.getElementById('fName');
-const lName = document.getElementById('lName');
-const eMail = document.getElementById('mail');
-const zipCode = document.getElementById('zip');
-const password = document.getElementById('pWord');
-const confirmPWord = document.getElementById('cPWord');
-const country = document.getElementById('country');
+const button = document.querySelector("button");
+const fName = document.getElementById("fName");
+const lName = document.getElementById("lName");
+const eMail = document.getElementById("mail");
+const zipCode = document.getElementById("zip");
+const password = document.getElementById("pWord");
+const confirmPWord = document.getElementById("cPWord");
+const country = document.getElementById("country");
+const form = document.querySelector('form');
 
 function checkZipCode() {
-  const countryCode = country.value
+  const countryCode = country.value;
   const constraints = {
     JP: [
-      '^\\d{3}-\\d{4}',
-      'The pattern for the postal code of Japan is ###-####, ex: 445-6543'
+      "^\\d{3}-\\d{4}",
+      "The pattern for the postal code of Japan is ###-####, ex: 445-6543",
     ],
     KR: [
-      '^\\d{5}',
-      'The pattern for the postal code of South Korea is #####, ex: 46704'
+      "^\\d{5}$",
+      "The pattern for the postal code of South Korea is #####, ex: 46704",
     ],
-    SG:[
-      '\\d{6}',
-      'The pattern for the postal code of Singapore is ######, ex: 798934'
-    ]
-  }
-  
+    SG: [
+      "^\\d{6}$",
+      "The pattern for the postal code of Singapore is ######, ex: 798934",
+    ],
+  };
+
   const constraint = new RegExp(constraints[countryCode][0]);
 
   if (constraint.test(zipCode.value)) {
-    zipCode.setCustomValidity('');
-    return true
-  } 
+    zipCode.setCustomValidity("");
+    return true;
+  }
   zipCode.setCustomValidity(constraints[countryCode][1]);
-  return false
-  
+  return false;
+}
+
+function checkPasswordPattern() {
+  const passwordPattern = new RegExp(
+    "^.*(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*\\W).*$"
+  );
+  if (passwordPattern.test(password.value)) {
+    
+    password.setCustomValidity("");
+    return true;
+  }
+  password.setCustomValidity(
+    "Your password must contain capital and small letters, numbers ,and special characters."
+  );
+  return false;
+}
+
+function confirmPassword() {
+  console.log(password.value === confirmPWord.value)
+  if (password.value === confirmPWord.value) {
+    confirmPWord.setCustomValidity("");
+    return true;
+  }
+  confirmPWord.setCustomValidity(
+    "Please enter the same entries with your initial password."
+  );
+  return false;
 }
 
 
-function validateForm (event) {
-  event.preventDefault();
+
+function validateForm(event) {
   event.stopPropagation();
+  checkPasswordPattern();
+  confirmPassword();
   if (!fName.checkValidity()) {
-    fName.classList.add('submitted');
+    fName.classList.add("submitted");
     fName.reportValidity();
   } else if (!lName.checkValidity()) {
     lName.reportValidity();
   } else if (!eMail.checkValidity()) {
-    eMail.reportValidity(); 
+    eMail.reportValidity();
   } else if (!checkZipCode(zipCode)) {
-    zipCode.reportValidity('');
+    zipCode.reportValidity("");
   } else if (!password.checkValidity()) {
     password.reportValidity();
+    checkPasswordPattern()
+  } else if (!checkPasswordPattern()) {
+    password.reportValidity();
+  } else if (!confirmPWord.checkValidity()) {
+    confirmPWord.reportValidity();
   } else if (!confirmPassword()) {
     confirmPWord.reportValidity();
   } else {
-    alert('Yey!!!')
+    alert("Yey!!!");
   }
-  
+  event.preventDefault();
 }
 
-button.addEventListener('click', validateForm)
+
+
+button.addEventListener("click", validateForm);
